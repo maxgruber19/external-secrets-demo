0ce7cbb40aa0d62bdfc35378605306a039d2cf04
Max Gruber <maxgruber19@gmail.com>
2024-04-18 00:31:25 +0200

    added vault kubernetes auth basics not working yet

---
+++ vault/kubernetes-auth/requirements/helm-requirements.sh
@@ -0,0 +1,7 @@
+helm repo add external-secrets https://charts.external-secrets.io
+helm repo add hashicorp https://helm.releases.hashicorp.com
+
+helm upgrade --install external-secrets external-secrets/external-secrets -n external-secrets --create-namespace
+helm upgrade --install vault hashicorp/vault --namespace vault --create-namespace
+
+kubectl create namespace vault-kubernetes-auth
