9b0ceb180a27a17b49917efab7e1d2f4d53b8a95
Max Gruber <maxgruber19@gmail.com>
2024-04-18 00:11:52 +0200

    initial commit

--- vault/token-auth/requirements/helm-requirements.sh
+++ vault/token-auth/requirements/helm-requirements.sh
@@ -3,3 +3,5 @@ helm repo add hashicorp https://helm.releases.hashicorp.com
 
 helm upgrade --install external-secrets external-secrets/external-secrets -n external-secrets --create-namespace
 helm upgrade --install vault hashicorp/vault --namespace vault --create-namespace
+
+kubectl create namespace vault-token-auth
